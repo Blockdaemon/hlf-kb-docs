4fd6d3f105995738a7fc9f72aa83fd648265a5b6
Nye Liu <nyet@nyet.org>
2019-06-21 15:51:39 -0700

    Add MacOS support

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -e
+
+OS=$(uname -s)
+if [ "$OS" == "Darwin" ]; then
+    BASE64="base64 -D"
+else
+    BASE64="base64 -d"
+fi
+
 . config.env
 
 curl -sSk https://ca-server.${NETWORK_ID}${ENV}.bdnodes.net:7054/api/v1/cainfo \
-  | jq -r ".result.CAChain" | base64 -d > tlsca-${NETWORK_ID}.pem
+  | jq -r ".result.CAChain" | ${BASE64} > tlsca-${NETWORK_ID}.pem
 
 export FABRIC_CA_CLIENT_TLS_CERTFILES="${PWD}/tlsca-${NETWORK_ID}.pem"
 export FABRIC_CA_CLIENT_CANAME="ca-peer-org"
